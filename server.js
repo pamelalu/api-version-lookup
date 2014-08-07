58eec30d4cbd2760463e0a4a89d2e52c7b210cd3
Pamela Lu-Stone <pstone@Pamelas-MacBook-Pro.local>
2014-08-07 14:52:15 -0700

    Added versions

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    version = require('./routes/versions');
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/versions', version.getVersions);
+
+/*app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);*/
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
